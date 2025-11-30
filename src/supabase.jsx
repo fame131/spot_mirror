9308c7c9fe368c8429d308aa5e0fa28d807cc393
fame <fame@gmail.com>
2025-11-30 14:25:49 +0530

    Add vercel.json to root for Vercel routing fix

--- src/supabase.jsx
+++ src/supabase.jsx
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = "https://dgndzwlaovcpwrrdotwm.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnbmR6d2xhb3ZjcHdycmRvdHdtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1MjQxOTYsImV4cCI6MjA3ODEwMDE5Nn0.VkjWijQT7swsEHIp3RyCBW4FEKzzA3Zw7pt70bTUzI8"
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
